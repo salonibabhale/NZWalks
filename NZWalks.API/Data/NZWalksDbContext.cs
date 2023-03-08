537ca491701b6fe9aadbcdd983019626aaedce9b
salonib <salonibabhale@gmail.com>
2023-03-08 18:50:30 +0530

    changes

---
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext:DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+        { 
+        
+        }
+
+    public DbSet<Region> Regions { get; set; }
+    public DbSet<Walk> Walks { get; set; }
+    public DbSet<Walkdifficulty> Walkdifficulty { get; set; }
+     }
+}
+
---
+++ NZWalks.API/Models/Domain/Walkdifficulty.cs
@@ -0,0 +1,15 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walkdifficulty
+    {
+        public Guid Id { get; set; }
+        public string? Code { get; set; }
+        public double Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkdifficultyId { get; set; }
+
+        //Navigation property 
+        public IEnumerable<Walk>? Walks { get; set; }
+
+    }
+}
