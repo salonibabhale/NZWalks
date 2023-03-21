94de5636bcd04b9be57312ccb713c7469fd05459
salonib <salonibabhale@gmail.com>
2023-03-21 15:45:30 +0530

    end of section 3

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+        private readonly IMapper mapper;
+
+        public RegionsController(IRegionRepository regionRepository, IMapper mapper)
+        {
+            this.regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+
+     
+
+        [HttpGet]
+        public IActionResult GetAllRegions()
+        {
+            var regions = regionRepository.GetAll;
+
+            //return DTO region 
+            //var regionsDTO = new List<Models.DTO.Region>();
+            //regions.ToList().ForEach(Region => 
+
+            //{
+            //  var regionDTO = new Models.DTO.Region() 
+            //{
+            //  Id = Region.Id,
+            //Code = Region.Code,
+            //Name = Region.Name,
+            //Area= Region.Area,
+            //Lat= Region.Lat,
+            //Long= Region.Long,
+            //Population = Region.Population,
+            //};
+            //regionsDTO.Add(regionDTO);
+            //});
+
+            var regionsDTO = mapper.Map<List<Models.DTO.Region>>(regions);
+
+            return Ok(regions);
+        }
+    }
+}
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using NZWalks.API.Data;
+using NZWalks.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,8 @@ builder.Services.AddDbContext<NZWalksDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalks"));
 
 });
+builder.Services.AddScoped<IRegionRepository, IRegionRepository>();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
